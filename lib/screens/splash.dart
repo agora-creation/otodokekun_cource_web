17232410c312a2d24b523965de78858a6d2eeeac
agoracreation <agora.creation.com@gmail.com>
2021-01-25 19:30:45 +0900

    first commit

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,8 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:otodokekun_cource_web/helpers/style.dart';
+import 'package:otodokekun_cource_web/widgets/loading.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: kMainColor,
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'お届けくん',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                ),
+              ),
+              SizedBox(height: 8.0),
+              Text(
+                '期間配達サービス',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                ),
+              ),
+              SizedBox(height: 24.0),
+              LoadingWidget(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/loading.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoadingWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CircularProgressIndicator(
+      valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+    );
+  }
+}
