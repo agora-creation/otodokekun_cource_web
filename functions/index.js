711e77b7dbdde069782b4f428776cf1b13b13b8c
agoracreation <agora.creation.com@gmail.com>
2021-03-05 14:09:57 +0900

    20210305 agoracreation commit

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,7 @@ admin.initializeApp(functions.config().firebase);
 let db = admin.firestore();
 
 let today = new Date();
+let today_7 = today.setDate(today.getDate() + 7);
 
 exports.deleteShopPlan = functions.region('asia-northeast1').pubsub.schedule('0 0 * * *').timeZone('Asia/Tokyo').onRun((_) => {
     let shopRef = db.collection('shop');
@@ -29,10 +30,26 @@ exports.deleteShopPlan = functions.region('asia-northeast1').pubsub.schedule('0 
 exports.updateShopPlan = functions.region('asia-northeast1').pubsub.schedule('0 0 * * 1').timeZone('Asia/Tokyo').onRun((_) => {
     let shopRef = db.collection('shop');
     let userRef = db.collection('user');
-    userRef.where('fixed' == true).get().then((userSnapshot) => {
+    userRef.where('fixed', '==', true).get().then((userSnapshot) => {
         userSnapshot.forEach((userDoc) => {
-            shopRef.doc(userDoc.data()['shopId']).collection('plan').get().then((planSnapshot) => {
-                console.log('data:');
+            shopRef.doc(userDoc.data()['shopId']).collection('plan').where('deliveryAt', '>=', today).where('deliveryAt', '<=', today_7).get().then((planSnapshot) => {
+                planSnapshot.forEach((planDoc) => {
+                    shopRef.doc(userDoc.data()['shopId']).collection('order').add({
+                        shopId: userDoc.data()['shopId'],
+                        userId: userDoc.id,
+                        name: userDoc.data()['name'],
+                        zip: userDoc.data()['zip'],
+                        address: userDoc.data()['address'],
+                        tel: userDoc.data()['tel'],
+                        cart: [],
+                        deliveryAt: planDoc.data()['deliveryAt'],
+                        remarks: '',
+                        totalPrice: 0,
+                        staff: userDoc.data()['staff'],
+                        shipping: false,
+                        createdAt: planDoc.data()['deliveryAt'],
+                    });
+                });
             });
         });
     });
