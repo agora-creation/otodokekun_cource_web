d4139b4e65131f9b0842446ed697f152f032f478
charakuri8 <charakuri8@gmail.com>
2021-03-06 17:05:41 +0900

    20210306 shimamura commit

--- functions/index.js
+++ functions/index.js
@@ -5,10 +5,8 @@ admin.initializeApp(functions.config().firebase);
 
 let db = admin.firestore();
 
-let today = new Date();
-let today_7 = today.setDate(today.getDate() + 7);
-
 exports.deleteShopPlan = functions.region('asia-northeast1').pubsub.schedule('0 0 * * *').timeZone('Asia/Tokyo').onRun((_) => {
+    let today = new Date();
     let shopRef = db.collection('shop');
     shopRef.get().then((shopSnapshot) => {
         shopSnapshot.forEach((shopDoc) => {
@@ -28,12 +26,25 @@ exports.deleteShopPlan = functions.region('asia-northeast1').pubsub.schedule('0 
 });
 
 exports.updateShopPlan = functions.region('asia-northeast1').pubsub.schedule('0 0 * * 1').timeZone('Asia/Tokyo').onRun((_) => {
+    let today = new Date();
+    let today_7 = new Date();
+    today_7.setDate(today_7.getDate() + 7);
     let shopRef = db.collection('shop');
     let userRef = db.collection('user');
     userRef.where('fixed', '==', true).get().then((userSnapshot) => {
         userSnapshot.forEach((userDoc) => {
             shopRef.doc(userDoc.data()['shopId']).collection('plan').where('deliveryAt', '>=', today).where('deliveryAt', '<=', today_7).get().then((planSnapshot) => {
                 planSnapshot.forEach((planDoc) => {
+                    let cart = [];
+                    cart.push({
+                        'id': planDoc.data()['id'],
+                        'name': planDoc.data()['name'],
+                        'image': planDoc.data()['image'],
+                        'unit': planDoc.data()['unit'],
+                        'price': planDoc.data()['price'],
+                        'quantity': 1,
+                        'totalPrice': planDoc.data()['price'],
+                    });
                     shopRef.doc(userDoc.data()['shopId']).collection('order').add({
                         shopId: userDoc.data()['shopId'],
                         userId: userDoc.id,
@@ -41,10 +52,10 @@ exports.updateShopPlan = functions.region('asia-northeast1').pubsub.schedule('0 
                         zip: userDoc.data()['zip'],
                         address: userDoc.data()['address'],
                         tel: userDoc.data()['tel'],
-                        cart: [],
+                        cart: cart,
                         deliveryAt: planDoc.data()['deliveryAt'],
                         remarks: '',
-                        totalPrice: 0,
+                        totalPrice: planDoc.data()['price'],
                         staff: userDoc.data()['staff'],
                         shipping: false,
                         createdAt: planDoc.data()['deliveryAt'],
