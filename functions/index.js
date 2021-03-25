8ebfc9eb86bf958573e5879869309077b2d33dd9
agoracreation <agora.creation.com@gmail.com>
2021-03-25 18:08:46 +0900

    20210325 agora commit

--- functions/index.js
+++ functions/index.js
@@ -52,75 +52,93 @@ exports.updateUser = functions.region('asia-northeast1').pubsub.schedule('0 0 * 
     let today = new Date();
 
     let shopRef = db.collection('shop');
+    let userRef = db.collection('user');
     shopRef.get().then((shopSnapshot) => {
         shopSnapshot.forEach((shopDoc) => {
-            shopRef.doc(shopDoc.id).collection('invoice').where('closedAt', '==', today).get().then((planSnapshot) => {
-                let userRef = db.collection('user');
+            shopRef.doc(shopDoc.id).collection('invoice').get().then((invoiceSnapshot) => {
+                invoiceSnapshot.forEach((invoiceDoc) => {
+                    let closedAt = invoiceDoc.data()['closedAt'];
+                    closedAt.setDate(closedAt.getDate() + 1);
+                    if(closedAt == today){
+                        userRef.where('shopId', '==', shopDoc.id).get().then((userSnapshot) => {
+                            userSnapshot.forEach((userDoc) => {
+                                let locations = userDoc.data()['locations'];
+                                let shopIdNew = '';
+                                let locationsNew = [];
+                                Object.keys(locations).forEach(key => {
+                                    if(locations['target'] == true){
+                                        shopIdNew = locations['id'];
+                                    }
+                                    locationsNew.push({
+                                        'id': locations['id'],
+                                        'name': locations['name'],
+                                        'target': false,
+                                    });
+                                });
+                                if(shopIdNew != ''){
+                                    userDoc.ref.update({
+                                        'shopId': shopIdNew,
+                                        'locations': locationsNew,
+                                        'staff': '',
+                                        'fixed': false,
+                                    });
+                                    shopRef.doc(shopDoc.id).collection('order').where('deliveryAt', '>', today).get().then((orderSnapshot) => {
+                                        if(orderSnapshot.empty) {
+                                            console.log('No matching documents.');
+                                            return;
+                                        }
+                                        orderSnapshot.forEach((orderDoc) => {
+                                            orderDoc.ref.delete();
+                                        });
+                                    }).catch((e) => {
+                                        console.log('Error getting documents', e);
+                                    });
+                                }
+                            });
+                        });
+                    }
+                });
+            }).catch((e) => {
                 userRef.where('shopId', '==', shopDoc.id).get().then((userSnapshot) => {
                     userSnapshot.forEach((userDoc) => {
-                        //userのlocationsを取得
-                        //locations内のtargetがtrueのみ取得
-                        //userのshopIdを変更
-                        //userのlocationsを変更(リセット)
-                        //userのfixedを変更(リセット)
-                        //orderを削除(today以降)
-                        //PUSH通知？
-
+                        let locations = userDoc.data()['locations'];
+                        let shopIdNew = '';
+                        let locationsNew = [];
+                        Object.keys(locations).forEach(key => {
+                            if(locations['target'] == true){
+                                shopIdNew = locations['id'];
+                            }
+                            locationsNew.push({
+                                'id': locations['id'],
+                                'name': locations['name'],
+                                'target': false,
+                            });
+                        });
+                        if(shopIdNew != ''){
+                            userDoc.ref.update({
+                                'shopId': shopIdNew,
+                                'locations': locationsNew,
+                                'staff': '',
+                                'fixed': false,
+                            });
+                            shopRef.doc(shopDoc.id).collection('order').where('deliveryAt', '>', today).get().then((orderSnapshot) => {
+                                if(orderSnapshot.empty) {
+                                    console.log('No matching documents.');
+                                    return;
+                                }
+                                orderSnapshot.forEach((orderDoc) => {
+                                    orderDoc.ref.delete();
+                                });
+                            }).catch((e) => {
+                                console.log('Error getting documents', e);
+                            });
+                        }
                     });
                 });
-
-
-            }).catch((e) => {
-                console.log('Error getting documents', e);
             });
         });
     });
 });
-
-//exports.updateShopPlan = functions.region('asia-northeast1').pubsub.schedule('0 0 * * *').timeZone('Asia/Tokyo').onRun((_) => {
-//    let shopRef = db.collection('shop');
-//    let userRef = db.collection('user');
-//    userRef.where('fixed', '==', true).get().then((userSnapshot) => {
-//        userSnapshot.forEach((userDoc) => {
-//            shopRef.doc(userDoc.data()['shopId']).get().then((shopDoc) => {
-//                let deliveryAt = new Date();
-//                deliveryAt.setDate(deliveryAt.getDate() + shopDoc.data()['cancelLimit']);
-//                shopRef.doc(userDoc.data()['shopId']).collection('plan').where('deliveryAt', '==', deliveryAt).get().then((planSnapshot) => {
-//                    planSnapshot.forEach((planDoc) => {
-//                        let cart = [];
-//                        cart.push({
-//                            'id': planDoc.data()['id'],
-//                            'name': planDoc.data()['name'],
-//                            'image': planDoc.data()['image'],
-//                            'unit': planDoc.data()['unit'],
-//                            'price': planDoc.data()['price'],
-//                            'quantity': 1,
-//                            'totalPrice': planDoc.data()['price'],
-//                        });
-//                        let orderId = shopRef.doc(userDoc.data()['shopId']).collection('order').doc().id;
-//                        shopRef.doc(userDoc.data()['shopId']).collection('order').add({
-//                            id: orderId,
-//                            shopId: userDoc.data()['shopId'],
-//                            userId: userDoc.id,
-//                            name: userDoc.data()['name'],
-//                            zip: userDoc.data()['zip'],
-//                            address: userDoc.data()['address'],
-//                            tel: userDoc.data()['tel'],
-//                            cart: cart,
-//                            deliveryAt: planDoc.data()['deliveryAt'],
-//                            remarks: '',
-//                            totalPrice: planDoc.data()['price'],
-//                            staff: userDoc.data()['staff'],
-//                            shipping: false,
-//                            createdAt: planDoc.data()['deliveryAt'],
-//                        });
-//                    });
-//                });
-//            });
-//        });
-//    });
-//});
-
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
